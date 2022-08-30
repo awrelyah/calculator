12cb894e8e6dd8ced2e99f770e84e7ebcf3847b7
awrelyah <aureliamihkk@gmail.com>
2022-08-30 18:40:24 +0300

    calculation, clear all and equal functions

--- main.js
+++ main.js
@@ -1,16 +1,102 @@
-let previousOp = document.querySelector('.previous-operand');
-let currentOp = document.querySelector('.current-operand');
+let previousDisplayNum = document.querySelector('.previous-operand');
+let currentDisplayNum= document.querySelector('.current-operand');
+
 let numbers = document.querySelectorAll('.num');
-let operands = document.querySelector('.op');
+let operators = document.querySelectorAll('.op');
+
 let clearBtn = document.getElementById('clear');
 let equalsBtn = document.getElementById('equals');
 
+let currentNum = "";
+let previousNum = "";
+let operator = "";
+let result = "";
+
+
+equalsBtn.addEventListener('click', getResult);
+clearBtn.addEventListener('click', clearDisplay);
+
+//clear all
+function clearDisplay(){
+    currentNum = "";
+    previousNum = "";
+    operator = "";
+    result = "";
+    currentDisplayNum.textContent = currentNum;
+    previousDisplayNum.textContent = previousNum;
+
+}
+
+//get clicked numbers
 numbers.forEach(el => {
     el.addEventListener('click', (e) =>{
-        changeDisplay(e.target.textContent);
+        changeNum(e.target.textContent);
     })
-});
+}); 
 
-function changeDisplay(number){
-    currentOp.textContent = number;
+//add clicked numbers to display
+function changeNum(number){  
+        if(currentNum.length <= 12){
+            currentNum += number;
+            currentDisplayNum.textContent = currentNum;
+        }
 }
+
+//get clicked operator
+operators.forEach(op =>{
+    op.addEventListener('click', (o)=>{
+        operate(o.target.textContent);
+
+    })
+})
+
+//change previous num, current num and display after clicking an operator
+function operate(op){
+    if(result !==  ''){
+        operator = op;
+        previousNum = result;
+        currentNum = '';
+        console.log(previousNum);
+        console.log(currentNum);
+        console.log(result);
+
+        previousDisplayNum.textContent = previousNum + ' ' + operator;
+        currentDisplayNum.textContent = currentNum;
+    } else {
+        operator = op; 
+        previousNum = currentNum;
+        previousDisplayNum.textContent = previousNum + ' ' + operator;
+        currentNum = '';
+        currentDisplayNum.textContent = '';
+    };
+    
+}
+
+
+//get the result
+function getResult(){
+    previousNum = parseFloat(previousNum);
+    currentNum = parseFloat(currentNum);
+    result = parseFloat(result);
+
+    if(operator === '+'){
+         result = previousNum + currentNum;
+    } else if (operator === '-'){
+        result = previousNum - currentNum;
+    } else if (operator === 'x'){
+        result = previousNum * currentNum;
+    } else if (operator === '÷'){
+        if (currentNum === 0){
+            result = 'Error';
+        } else {
+            result = previousNum / currentNum;
+        }
+    }
+    previousDisplayNum.textContent = previousNum + operator + currentNum;
+    currentDisplayNum.textContent = result;
+
+
+}
+
+
+
